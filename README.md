325fcd713ae3e0f752c47b3108d2778f68be33dd
Florian Brandner <florian@brandner.net>
2024-11-24 03:09:08 +0100

    Add: Camera and HTTP Server

--- README.md
+++ README.md
@@ -8,19 +8,23 @@ A mobile ESP32 Camera with GPS Receiver. The camera image can be accessed as sna
 
 ### OV5640 ESP32 CAM
 
+A ESP32 Module from [Aliexpress](https://de.aliexpress.com/item/1005004227739159.html?gatewayAdapt=glo2deu) with a OV5640 5 MP sensor.
+
 ### Ublox GY-NEO6MV2 GPS Modul NEO-6M
 
 ## Software
 
 ## TODO
 
-- [ ] Base System
-- [ ] WiFi Client
+- [X] Base System
+- [X] WiFi Client
 - [ ] Fallback: WiFi Access Point
-- [ ] HTTP Server
-- [ ] Cam: Snapshot
-- [ ] Cam: Stream
-- [ ] MQTT Connection
+- [X] HTTP Server
+- [X] Cam: Snapshot
+- [X] Cam: Stream
+- [X] MQTT Connection
+- [ ] MQTT: Publish fw info
+- [ ] MQTT: Publish GPS data
 - [ ] MQTT: Cyclic image publish
 - [ ] MQTT: Image on request
 - [ ] GPS: Connectivity
--- main/main.c
+++ main/main.c
@@ -7,14 +7,19 @@
  */
 
 /* Includes ------------------------------------------------------------------*/
+#include <esp_camera.h>
 #include <stdio.h>
 #include <string.h>
 #include <time.h>
 
 /* Private includes ----------------------------------------------------------*/
+#include "esp_camera.h"
+#include "esp_check.h"
 #include "esp_chip_info.h"
 #include "esp_err.h"
+#include "esp_http_server.h"
 #include "esp_log.h"
+#include "esp_timer.h"
 #include "freertos/FreeRTOS.h"
 #include "mqtt.h"
 #include "nvs_flash.h"
@@ -22,16 +27,237 @@
 
 /* Private typedef -----------------------------------------------------------*/
 
+typedef struct {
+  httpd_req_t *req;
+  size_t len;
+} jpg_chunking_t;
+
 /* Private define ------------------------------------------------------------*/
+
+#define CAM_PIN_RESET 5
+#define CAM_PIN_XCLK 15
+#define CAM_PIN_SIOD 22
+#define CAM_PIN_SIOC 23
+#define CAM_PIN_D7 39
+#define CAM_PIN_D6 34
+#define CAM_PIN_D5 33
+#define CAM_PIN_D4 27
+#define CAM_PIN_D3 12
+#define CAM_PIN_D2 35
+#define CAM_PIN_D1 14
+#define CAM_PIN_D0 2
+#define CAM_PIN_VSYNC 18
+#define CAM_PIN_HREF 36
+#define CAM_PIN_PCLK 26
+
+#define PART_BOUNDARY "123456789000000000000987654321"
 
 /* Private macro -------------------------------------------------------------*/
 
 /* Private variables ---------------------------------------------------------*/
+
 static const char *TAG = "MAIN";
+
+static camera_config_t camera_config = {
+    .pin_pwdn = -1, // Unused
+    .pin_reset = CAM_PIN_RESET,
+    .pin_xclk = CAM_PIN_XCLK,
+    .pin_sccb_sda = CAM_PIN_SIOD,
+    .pin_sccb_scl = CAM_PIN_SIOC,
+    .pin_d7 = CAM_PIN_D7,
+    .pin_d6 = CAM_PIN_D6,
+    .pin_d5 = CAM_PIN_D5,
+    .pin_d4 = CAM_PIN_D4,
+    .pin_d3 = CAM_PIN_D3,
+    .pin_d2 = CAM_PIN_D2,
+    .pin_d1 = CAM_PIN_D1,
+    .pin_d0 = CAM_PIN_D0,
+    .pin_vsync = CAM_PIN_VSYNC,
+    .pin_href = CAM_PIN_HREF,
+    .pin_pclk = CAM_PIN_PCLK,
+    .xclk_freq_hz = 16000000, // 16 MHz
+    .ledc_timer = LEDC_TIMER_0,
+    .ledc_channel = LEDC_CHANNEL_0,
+    .pixel_format = PIXFORMAT_JPEG,
+    .frame_size = FRAMESIZE_SXGA,
+    .jpeg_quality = 5,
+    .fb_count = 1,
+    .grab_mode = CAMERA_GRAB_WHEN_EMPTY,
+};
+
+static const char *_STREAM_CONTENT_TYPE =
+    "multipart/x-mixed-replace;boundary=" PART_BOUNDARY;
+static const char *_STREAM_BOUNDARY = "\r\n--" PART_BOUNDARY "\r\n";
+static const char *_STREAM_PART =
+    "Content-Type: image/jpeg\r\nContent-Length: %u\r\n\r\n";
 
 /* Private function prototypes -----------------------------------------------*/
 
+// Callback to encode the JPEG stream
+static size_t jpg_encode_stream(void *arg, size_t index, const void *data,
+                                size_t len);
+
+// HTTP Request: Get a JPG Snapshot
+static esp_err_t stream_handler(httpd_req_t *req);
+
+// HTTP Request: Get a JPG Stream
+static esp_err_t snapshot_handler(httpd_req_t *req);
+
+// Start the HTTPd and register URIs
+static httpd_handle_t start_webserver();
+
 /* Private user code ---------------------------------------------------------*/
+
+// The URIs
+static const httpd_uri_t snapshot = {
+    .uri = "/snapshot",
+    .method = HTTP_GET,
+    .handler = snapshot_handler,
+};
+static const httpd_uri_t stream = {
+    .uri = "/stream",
+    .method = HTTP_GET,
+    .handler = stream_handler,
+};
+
+static size_t jpg_encode_stream(void *arg, size_t index, const void *data,
+                                size_t len) {
+  jpg_chunking_t *j = (jpg_chunking_t *)arg;
+  if (!index) {
+    j->len = 0;
+  }
+  if (httpd_resp_send_chunk(j->req, (const char *)data, len) != ESP_OK) {
+    return 0;
+  }
+  j->len += len;
+  return len;
+}
+
+static esp_err_t snapshot_handler(httpd_req_t *req) {
+  camera_fb_t *fb = NULL;
+  esp_err_t res = ESP_OK;
+  size_t fb_len = 0;
+  int64_t fr_start = esp_timer_get_time();
+
+  fb = esp_camera_fb_get();
+  if (!fb) {
+    ESP_LOGE(TAG, "Camera capture failed");
+    httpd_resp_send_500(req);
+    return ESP_FAIL;
+  }
+  res = httpd_resp_set_type(req, "image/jpeg");
+  if (res == ESP_OK) {
+    res = httpd_resp_set_hdr(req, "Content-Disposition",
+                             "inline; filename=capture.jpg");
+  }
+
+  if (res == ESP_OK) {
+    if (fb->format == PIXFORMAT_JPEG) {
+      fb_len = fb->len;
+      res = httpd_resp_send(req, (const char *)fb->buf, fb->len);
+    } else {
+      jpg_chunking_t jchunk = {req, 0};
+      res =
+          frame2jpg_cb(fb, 80, jpg_encode_stream, &jchunk) ? ESP_OK : ESP_FAIL;
+      httpd_resp_send_chunk(req, NULL, 0);
+      fb_len = jchunk.len;
+    }
+  }
+  esp_camera_fb_return(fb);
+  int64_t fr_end = esp_timer_get_time();
+  ESP_LOGI(TAG, "JPG: %luKB %lums", (uint32_t)(fb_len / 1024),
+           (uint32_t)((fr_end - fr_start) / 1000));
+
+  return res;
+}
+
+static esp_err_t stream_handler(httpd_req_t *req) {
+  camera_fb_t *fb = NULL;
+  esp_err_t res = ESP_OK;
+  size_t _jpg_buf_len;
+  uint8_t *_jpg_buf;
+  char *part_buf[64];
+  static int64_t last_frame = 0;
+  if (!last_frame) {
+    last_frame = esp_timer_get_time();
+  }
+
+  res = httpd_resp_set_type(req, _STREAM_CONTENT_TYPE);
+  if (res != ESP_OK) {
+    return res;
+  }
+
+  while (true) {
+    fb = esp_camera_fb_get();
+    if (!fb) {
+      ESP_LOGE(TAG, "Camera capture failed");
+      res = ESP_FAIL;
+      break;
+    }
+    if (fb->format != PIXFORMAT_JPEG) {
+      bool jpeg_converted = frame2jpg(fb, 80, &_jpg_buf, &_jpg_buf_len);
+      if (!jpeg_converted) {
+        ESP_LOGE(TAG, "JPEG compression failed");
+        esp_camera_fb_return(fb);
+        res = ESP_FAIL;
+      }
+    } else {
+      _jpg_buf_len = fb->len;
+      _jpg_buf = fb->buf;
+    }
+
+    if (res == ESP_OK) {
+      res = httpd_resp_send_chunk(req, _STREAM_BOUNDARY,
+                                  strlen(_STREAM_BOUNDARY));
+    }
+    if (res == ESP_OK) {
+      size_t hlen = snprintf((char *)part_buf, 64, _STREAM_PART, _jpg_buf_len);
+
+      res = httpd_resp_send_chunk(req, (const char *)part_buf, hlen);
+    }
+    if (res == ESP_OK) {
+      res = httpd_resp_send_chunk(req, (const char *)_jpg_buf, _jpg_buf_len);
+    }
+    if (fb->format != PIXFORMAT_JPEG) {
+      free(_jpg_buf);
+    }
+    esp_camera_fb_return(fb);
+    if (res != ESP_OK) {
+      break;
+    }
+    int64_t fr_end = esp_timer_get_time();
+    int64_t frame_time = fr_end - last_frame;
+    last_frame = fr_end;
+    frame_time /= 1000;
+    ESP_LOGI(TAG, "MJPG: %luKB %lums (%.1ffps)",
+             (uint32_t)(_jpg_buf_len / 1024), (uint32_t)frame_time,
+             1000.0 / (uint32_t)frame_time);
+  }
+
+  last_frame = 0;
+  return res;
+}
+
+static httpd_handle_t start_webserver(void) {
+  httpd_handle_t server = NULL;
+  httpd_config_t config = HTTPD_DEFAULT_CONFIG();
+
+  config.lru_purge_enable = true;
+
+  // Start the httpd server
+  ESP_LOGI(TAG, "Starting server on port: '%d'", config.server_port);
+  if (httpd_start(&server, &config) == ESP_OK) {
+    // Set URI handlers
+    ESP_LOGI(TAG, "Registering URI handlers");
+    httpd_register_uri_handler(server, &snapshot);
+    httpd_register_uri_handler(server, &stream);
+
+    return server;
+  }
+
+  ESP_LOGI(TAG, "Error starting server!");
+  return NULL;
+}
 
 /* Public user code ----------------------------------------------------------*/
 
@@ -101,13 +327,16 @@ void app_main(void) {
 
   if (ESP_OK == WiFi_Init()) {
     MQTT_Init();
+    start_webserver();
   } else {
     // TODO: If WiFi init failed, re-init in AP mode
   }
 
-  ESP_LOGW(TAG, "Entering loop");
+  ESP_ERROR_CHECK(esp_camera_init(&camera_config));
+
+  ESP_LOGI(TAG, "Entering loop");
   while (1) {
-    vTaskDelay(pdMS_TO_TICKS(30000));
-    ESP_LOGW(TAG, ".");
+    vTaskDelay(pdMS_TO_TICKS(10000));
+    ESP_LOGI(TAG, ".");
   }
 }
